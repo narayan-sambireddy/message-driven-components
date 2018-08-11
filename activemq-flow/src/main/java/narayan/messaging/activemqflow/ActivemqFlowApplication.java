6f96e038ed4468acae76135bc8fcfc1659c5c6ba
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-08-12 02:49:18 +0530

    Added activemq-flow

---
+++ activemq-flow/src/main/java/narayan/messaging/activemqflow/ActivemqFlowApplication.java
@@ -0,0 +1,12 @@
+package narayan.messaging.activemqflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqFlowApplication.class, args);
+	}
+}
---
+++ activemq-flow/src/test/java/narayan/messaging/activemqflow/ActivemqFlowApplicationTests.java
@@ -0,0 +1,16 @@
+package narayan.messaging.activemqflow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivemqFlowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
